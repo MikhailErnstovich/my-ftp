f14bc15e05a3277b31c951e3a8222b376e987f23
Mikhail <mr.nimostor@mail.ru>
2022-04-10 12:56:34 +0300

    fix

--- custom.js
+++ custom.js
@@ -14,7 +14,8 @@
 
 ******************************/
 
-$(document).ready(function () {
+$(document).ready(() => setTimeout(
+	function () {
 	"use strict";
 
 	/* 
@@ -40,7 +41,7 @@ $(document).ready(function () {
 	initHomeSlider();
 	initSearch();
 	initMenu();
-	// initIsotope();
+	initIsotope();
 
 	/* 
 
@@ -223,39 +224,39 @@ $(document).ready(function () {
 
 	*/
 
-	// function initIsotope() {
-	// 	var sortingButtons = $('.product_sorting_btn');
-	// 	var sortNums = $('.num_sorting_btn');
+	function initIsotope() {
+		var sortingButtons = $('.product_sorting_btn');
+		var sortNums = $('.num_sorting_btn');
 
-	// 	if ($('.product_grid').length) {
-	// 		var grid = $('.product_grid').isotope({
-	// 			itemSelector: '.product',
-	// 			layoutMode: 'fitRows',
-	// 			fitRows:
-	// 			{
-	// 				gutter: 30
-	// 			},
-	// 			getSortData:
-	// 			{
-	// 				price: function (itemElement) {
-	// 					var priceEle = $(itemElement).find('.product_price').text().replace('$', '');
-	// 					return parseFloat(priceEle);
-	// 				},
-	// 				name: '.product_name',
-	// 				stars: function (itemElement) {
-	// 					var starsEle = $(itemElement).find('.rating');
-	// 					var stars = starsEle.attr("data-rating");
-	// 					return stars;
-	// 				}
-	// 			},
-	// 			animationOptions:
-	// 			{
-	// 				duration: 750,
-	// 				easing: 'linear',
-	// 				queue: false
-	// 			}
-	// 		});
-	// 	}
-	// }
+		if ($('.product_grid').length) {
+			var grid = $('.product_grid').isotope({
+				itemSelector: '.product',
+				layoutMode: 'fitRows',
+				fitRows:
+				{
+					gutter: 30
+				},
+				getSortData:
+				{
+					price: function (itemElement) {
+						var priceEle = $(itemElement).find('.product_price').text().replace('$', '');
+						return parseFloat(priceEle);
+					},
+					name: '.product_name',
+					stars: function (itemElement) {
+						var starsEle = $(itemElement).find('.rating');
+						var stars = starsEle.attr("data-rating");
+						return stars;
+					}
+				},
+				animationOptions:
+				{
+					duration: 750,
+					easing: 'linear',
+					queue: false
+				}
+			});
+		}
+	}
 
-});
+}, 3000));
